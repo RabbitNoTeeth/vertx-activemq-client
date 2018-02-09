f0fbb7dc685b12120e573740787da1010a5709ab
don9 <864426487@qq.com>
2018-02-09 14:49:36 +0800

    Session缓存池优化

--- src/main/java/fun/bookish/vertx/activemq/client/pool/ActiveMQSessionPool.java
+++ src/main/java/fun/bookish/vertx/activemq/client/pool/ActiveMQSessionPool.java
@@ -12,31 +12,30 @@ public class ActiveMQSessionPool {
 
     private final Connection connection;
     private final int poolSize;
-    private final ConcurrentHashMap<Integer,Session> pool;
+    private static final ConcurrentHashMap<Integer,Session> pool = new ConcurrentHashMap<>();
 
 
     public ActiveMQSessionPool(Connection connection,int poolSize){
         this.connection = connection;
-        this.poolSize = poolSize;
         if(poolSize > 1) {
-            this.pool = new ConcurrentHashMap<>(poolSize);
+            this.poolSize = poolSize;
         }else{
-            this.pool = new ConcurrentHashMap<>(DEFAULT_POOL_SIZE);
+            this.poolSize = DEFAULT_POOL_SIZE;
         }
 
     }
 
-    private final static int DEFAULT_POOL_SIZE = 8;
+    private final static int DEFAULT_POOL_SIZE = 6;
     private final static Random random = new Random();
 
 
     public Session getSession(){
         int randomKey = random.nextInt(DEFAULT_POOL_SIZE);
-        Session session = this.pool.get(randomKey);
+        Session session = pool.get(randomKey);
         try {
             if(session == null){
                 Session newSession = this.connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-                if(this.pool.putIfAbsent(randomKey,newSession) == null){
+                if(pool.putIfAbsent(randomKey,newSession) == null){
                     session = newSession;
                 }else{
                     newSession.close();
